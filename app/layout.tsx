6a399c91346b8bb7bcf6dbd19d1b0ed743853292
ynncstslv <ynncstslv@gmail.com>
2023-10-20 12:39:19 -0300

    fix: changing default font-family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from 'next';
 
 import './globals.css';
-import { Inter } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const poppins = Poppins({
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+	subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
 	title: 'Gathr | Expand you collection!',
@@ -18,7 +21,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={poppins.className}>{children}</body>
 		</html>
 	);
 }
