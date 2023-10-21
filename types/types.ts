f620e933d8f9ac105d5b2000ff0e87d541d80eeb
ynncstslv <ynncstslv@gmail.com>
2023-10-21 19:29:35 -0300

    feat: declaring types

---
+++ types/types.ts
@@ -0,0 +1,19 @@
+import { StaticImageData } from 'next/image';
+
+export type CartProductType = {
+	id: string;
+	name: string;
+	description: string;
+	category: string;
+	brand?: string;
+	selectedImage: SelectedImageType;
+	stock: number;
+	quantity: number;
+	price: number;
+};
+
+export type SelectedImageType = {
+	color: string;
+	colorCode: string;
+	image: StaticImageData;
+};
