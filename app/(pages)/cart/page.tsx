f07179c2b776d48420b36e2c32f0756711acd69b
ynncstslv <ynncstslv@gmail.com>
2023-10-22 20:58:34 -0300

    fix: small padding on cart page

--- app/(pages)/cart/page.tsx
+++ app/(pages)/cart/page.tsx
@@ -3,7 +3,7 @@ import Container from '@/components/Container';
 
 export default function CartPage() {
 	return (
-		<main className="pt-4 pb-14 md:py-24">
+		<main className="pt-4 pb-14 md:py-16">
 			<Container>
 				<Cart />
 			</Container>
