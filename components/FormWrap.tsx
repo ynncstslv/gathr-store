305f70d57b5e739a78ae982264e4a9cb9e29e8ed
ynncstslv <ynncstslv@gmail.com>
2023-10-23 13:14:03 -0300

    feat: create form wrapper component

---
+++ components/FormWrap.tsx
@@ -0,0 +1,11 @@
+const FormWrap = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<div className="h-full min-h-fit flex items-center justify-center">
+			<div className="w-full min-w-[350px] flex flex-col gap-6 p-8 rounded-xl bg-neutral-50 shadow-xl md:min-w-[400px]">
+				{children}
+			</div>
+		</div>
+	);
+};
+
+export default FormWrap;
