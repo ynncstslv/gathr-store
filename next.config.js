cbc1b844999287f57f82f4dc8f3669150c8cd5e9
ynncstslv <ynncstslv@gmail.com>
2023-10-24 15:23:54 -0300

    fix: adding github image link to next.config and prisma generate to package

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['cards.scryfall.io', 'lh3.googleusercontent.com'],
+		domains: [
+			'cards.scryfall.io',
+			'lh3.googleusercontent.com',
+			'avatars.githubusercontent.com',
+		],
 	},
 };
 
