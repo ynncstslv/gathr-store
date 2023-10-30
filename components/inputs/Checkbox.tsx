6ab8b9ac0b34ddbfb27e6acb94e61f7c5b988851
ynncstslv <ynncstslv@gmail.com>
2023-10-30 15:01:27 -0300

    feat: create checkbox component

---
+++ components/inputs/Checkbox.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { FC } from 'react';
+import { FieldValues, UseFormRegister } from 'react-hook-form';
+
+interface CheckboxProps {
+	id: string;
+	label: string;
+	disabled?: boolean;
+	register: UseFormRegister<FieldValues>;
+}
+
+const Checkbox: FC<CheckboxProps> = ({ id, label, disabled, register }) => {
+	return (
+		<div className="w-full flex items-center gap-2">
+			<input id={id} type="checkbox" disabled={disabled} {...register(id)} />
+			<label htmlFor={id} className="font-light text-sm text-neutral-500">
+				{label}
+			</label>
+		</div>
+	);
+};
+
+export default Checkbox;
