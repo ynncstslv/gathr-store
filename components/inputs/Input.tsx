6179b51d87ba968a9d22c74d1cb9352b699bc457
ynncstslv <ynncstslv@gmail.com>
2023-10-30 15:01:50 -0300

    fix: altering input component

--- components/inputs/Input.tsx
+++ components/inputs/Input.tsx
@@ -24,7 +24,7 @@ const Input: FC<InputProps> = ({
 	errors,
 }) => {
 	return (
-		<div className="">
+		<div className="w-full">
 			<label
 				htmlFor={id}
 				className="block mb-1 text-sm leading-6 text-violet-900"
@@ -36,7 +36,7 @@ const Input: FC<InputProps> = ({
 				type={type}
 				autoComplete="off"
 				className={cn(
-					`w-full block px-3 py-2 mb-4 text-neutral-900 border rounded-md border-neutral-300 placeholder:text-neutral-400 focus:ring-1 focus:ring-inset focus:ring-violet-200 sm:text-sm sm:leading-6`,
+					`w-full px-3 py-2 mb-4 text-neutral-900 border rounded-md border-neutral-300 placeholder:text-neutral-400 focus:ring-1 focus:ring-inset focus:ring-violet-200 sm:text-sm sm:leading-6`,
 					errors[id] && 'focus:ring-rose-500',
 					disabled && 'opacity-50 cursor-not-allowed'
 				)}
