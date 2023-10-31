14081f1212ad2a8aac613bc9a6626dae6a3e65f7
ynncstslv <ynncstslv@gmail.com>
2023-10-31 16:06:26 -0300

    fix: get product by id to display product page

--- app/(pages)/store/[productId]/page.tsx
+++ app/(pages)/store/[productId]/page.tsx
@@ -1,4 +1,6 @@
+import getProductById from '@/actions/getProductById';
 import Container from '@/components/Container';
+import NullData from '@/components/NullData';
 import ProductDetails from '@/components/ProductDetails';
 import ProductRatings from '@/components/ProductRatings';
 import { dummyProducts } from '@/lib/dummy';
@@ -7,8 +9,12 @@ interface IParams {
 	productId?: string;
 }
 
-export default function ProductPage({ params }: { params: IParams }) {
-	const product = dummyProducts.find((p) => p.id === params.productId);
+export default async function ProductPage({ params }: { params: IParams }) {
+	const product = await getProductById(params);
+
+	if (!product) {
+		return <NullData title="Product not found!" />;
+	}
 
 	return (
 		<main className="pt-4 pb-14 md:py-24">
