e559b2a0f986937d7e74c5e2941f6bd054ff2c31
ynncstslv <ynncstslv@gmail.com>
2023-10-31 13:23:38 -0300

    feat: create getProducts helper function

---
+++ actions/getProducts.ts
@@ -0,0 +1,60 @@
+import prisma from '@/lib/prismadb';
+
+export interface IProductParams {
+	category?: string | null;
+	searchTerm?: string | null;
+}
+
+export default async function getProducts(params: IProductParams) {
+	try {
+		const { category, searchTerm } = params;
+
+		let searchString = searchTerm;
+
+		if (!searchTerm) {
+			searchString = '';
+		}
+
+		let query: any = {};
+
+		if (category) {
+			query.category = category;
+		}
+
+		const products = await prisma.product.findMany({
+			where: {
+				...query,
+				OR: [
+					{
+						name: {
+							contain: searchString,
+							mode: 'insensitive',
+						},
+						set: {
+							contain: searchString,
+							mode: 'insensitive',
+						},
+						description: {
+							contain: searchString,
+							mode: 'insensitive',
+						},
+					},
+				],
+			},
+			include: {
+				reviews: {
+					include: {
+						user: true,
+					},
+					orderBy: {
+						createdAt: 'desc',
+					},
+				},
+			},
+		});
+
+		return products;
+	} catch (error: any) {
+		throw new Error(error);
+	}
+}
