6c6d9dbfbf1e532278baea46745a0e1df701c23c
ynncstslv <ynncstslv@gmail.com>
2023-10-31 21:51:16 -0300

    fix: removing categories from store page since not using

--- app/(pages)/store/page.tsx
+++ app/(pages)/store/page.tsx
@@ -1,13 +1,11 @@
 import Container from '@/components/Container';
 import Hero from '@/components/landing/Hero';
-import Categories from '@/components/store/Categories';
 import Products from '@/components/store/Products';
 
 export default function StorePage() {
 	return (
 		<main className="py-6 md:py-18">
 			<Container>
-				<Categories />
 				<Products />
 			</Container>
 		</main>
