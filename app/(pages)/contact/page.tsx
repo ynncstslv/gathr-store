64033a767a25aa32f9c302c4f5cc9996f5177066
ynncstslv <ynncstslv@gmail.com>
2023-11-01 13:55:28 -0300

    feat: create about and contact pages but with null data component

---
+++ app/(pages)/contact/page.tsx
@@ -0,0 +1,12 @@
+import Container from '@/components/Container';
+import NullData from '@/components/NullData';
+
+export default function AboutPage() {
+	return (
+		<main className="py-6 md:py-18">
+			<Container>
+				<NullData title="This page is under construction." />
+			</Container>
+		</main>
+	);
+}
